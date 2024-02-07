8c2f890d3171ea83dee767bfb481b316377cca41
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-02-07 05:43:57 +0200

    loggerMiddleware implemented

---
+++ logger.middleware.ts
@@ -0,0 +1,44 @@
+'use strict';
+import { Request, Response, NextFunction } from 'express';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const startAt = process.hrtime();
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+      const diff = process.hrtime(startAt);
+      const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${responseTime}ms ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
+    const send = response.send;
+    response.send = (exitData) => {
+      if (
+        response
+          ?.getHeader('content-type')
+          ?.toString()
+          .includes('application/json')
+      ) {
+        console.log({
+          code: response.statusCode,
+          exit: exitData.toString().substring(0, 1000),
+          endDate: new Date(),
+        });
+      }
+      response.send = send;
+      return response.send(exitData);
+    };
+
+    next();
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from 'logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
