96a55dddaa421876ebd91582421dff4e31c7dff5
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-02-08 05:11:14 +0200

    connected to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from 'logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConnectionService } from './database-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database-connection.service.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+export class DatabaseConnectionService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(
+    connectionName?: string,
+  ): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      name: 'default',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD.toString(),
+      database: process.env.DATABASE_DB,
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+      entities: ['dist/**/*.entity.js'],
+    };
+  }
+}
